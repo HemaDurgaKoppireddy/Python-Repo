d37d4a1844d46766a1bbb41cc438e3ffb1dfbf62
Hema Durga Koppireddy <23P31A0530@acet.ac.in>
2024-10-05 14:39:45 +0530

    Uploading code: Kmph to Mps.py

---
+++ Kmph to Mps.py
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class KM{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        float n = sc.nextFloat();
+        float res = n*5/18;
+        System.out.printf("%.2f",res);
+    }
+}
